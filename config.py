d0ea05081609937509cd2ba65e25b71f7a930438
Xflixbro <yedhuku2004@gmail.com>
2026-09-14 01:57:11 +0530

    Update config.py
    Hy

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ BOT_TOKEN = "8919663861:AAE6doj5mSDlO2G5XY5CVs_aN7kgS3HRJKg"
 # ===========================
 
 # MongoDB Connection String
-DATABASE_URI = "mongodb+srv://eonxstBotv1:eonxstBotv1@cluster0.svu9vwp.mongodb.net/?appName=Cluster0"
+DATABASE_URI = "mongodb+srv://yedhuku2002_db_user:BDUZlXAnowC6Ybqt@cluster0.tqjuuyt.mongodb.net/?appName=Cluster0"
 DATABASE_NAME = "CORNXVILLA"
 
 # ===========================
