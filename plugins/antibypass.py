1650c94c5d32c5f3af5cea6d128b9c61a0eb08a8
Xflixbro <yedhuku2004@gmail.com>
2026-09-17 00:15:58 +0530

    Update antibypass.py
    Hj

--- plugins/antibypass.py
+++ plugins/antibypass.py
@@ -11,7 +11,7 @@ from pyrogram import Client
 # =====================================================
 GATEWAY_BASE_URL = "https://oggyflix2.vercel.app"
 
-MIN_SOLVE_TIME = 2
+MIN_SOLVE_TIME = 1
 ACCESS_TOKEN_EXPIRY_MINUTES = 10
 
 
